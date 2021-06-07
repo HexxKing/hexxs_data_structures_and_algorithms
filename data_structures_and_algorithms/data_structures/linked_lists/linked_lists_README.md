2cd792d47162883dfe47cee2c67890b2fbc62213
Hexx King <hexxking13@gmail.com>
2021-06-07 13:36:05 -0700

    commiting a bunch of work to set up other data structures - starting with a clean commit slate

--- data_structures_and_algorithms/data_structures/linked_lists/linked_lists_README.md
+++ data_structures_and_algorithms/data_structures/linked_lists/linked_lists_README.md
@@ -1,4 +1,4 @@
-# ✨ Summary
+# ✨ Summary - Singly Linked Lists
 
 # ✨ Description
 
--- tests/test_linked_lists.py
+++ tests/test_linked_lists.py
@@ -1,5 +1,5 @@
 import pytest
-from data_structures_and_algorithms.example_catagory.example_challenge.example_challenge import hello_world
+from data_structures_and_algorithms.data_structures.linked_lists.linked_lists import hello_world
 
 #Test Connection 
 def test_hello_world():
